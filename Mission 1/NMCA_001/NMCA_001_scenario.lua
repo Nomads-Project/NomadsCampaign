2becba64525befca3df261b36831f995ddeea843
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-04 17:36:24 +0100

    add mission 1 files

---
+++ Mission 1/NMCA_001/NMCA_001_scenario.lua
@@ -0,0 +1,27 @@
+version = 3
+ScenarioInfo = {
+    name = 'Mission1',
+    description = '',
+    type = 'campaign_coop',
+    starts = true,
+    preview = '',
+    size = {512, 512},
+    map = '/maps/NMCA_001/NMCA_001.scmap',
+    save = '/maps/NMCA_001/NMCA_001_save.lua',
+    script = '/maps/NMCA_001/NMCA_001_script.lua',
+    norushradius = 0.000000,
+    norushoffsetX_Player1 = 0.000000,
+    norushoffsetY_Player1 = 0.000000,
+    norushoffsetX_UEF = 0.000000,
+    norushoffsetY_UEF = 0.000000,
+    norushoffsetX_Civilian = 0.000000,
+    norushoffsetY_Civilian = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {'Player1','UEF','Civilian',} },
+            },
+            customprops = {
+            },
+        },
+    }}
---
+++ Mission 1/NMCA_001/NMCA_001_script.lua
@@ -0,0 +1,13 @@
+local Objectives = import('/lua/ScenarioFramework.lua').Objectives
+local ScenarioFramework = import('/lua/ScenarioFramework.lua')
+local ScenarioPlatoonAI = import('/lua/ScenarioPlatoonAI.lua')
+local ScenarioUtils = import('/lua/sim/ScenarioUtilities.lua')
+local Utilities = import('/lua/utilities.lua')
+  
+function OnPopulate()
+	ScenarioUtils.InitializeScenarioArmies()
+end
+  
+function OnStart(self)
+	
+end
