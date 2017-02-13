77b2b9d71dcd23352b10fcb40ea4dd8f78438a8a
JJ173 <jack.t371@hotmail.co.uk>
2017-02-13 14:19:55 +0000

    Latest Version of Nomad Mission 1
    Here is the mission ready to be reviewed in it's current state. (Not finished yet). Still have some things to improve on before release.

---
+++ NMCA_001/NMCA_001_strings.lua
@@ -0,0 +1,101 @@
+IntroNIS_Dialogue = {
+	{text = "[Nichols]: Alright, we've landed safely! Nice job sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_01', faction = 'UEF'},
+	{text = "[Nichols]: Our men have dispatched our Engineers. They are now under your control.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_02', faction = 'UEF'},
+	{text = "[Nichols]: I'd recommend constructing a base and scouting the area sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_03', faction = 'UEF'},
+	{text = "[Nichols]: We're limited on units sir, choose your approach carefully. Nichols out.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_05', faction = 'UEF'},
+}
+
+UEF_Notice = {
+	{text = "[Parker]: What the hell? You're in a restricted area! You're under arrest!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_01', faction = 'UEF'},
+	{text = "[Nichols]: Ah hell. We've been spotted! We need to get out of here. Fire up the engines!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_06', faction = 'UEF'},
+	{text = "[Parker]: Not on my watch!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_02', faction = 'UEF'},
+}
+
+M1_Scream_Incoming = {
+	{text = "[Nichols]: INCOMING!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_07', faction = 'UEF'},
+}
+
+M1_Update_Commander = {
+	{text = "[Nichols]: Ah hell, that missile knocked the engines out of commision!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_08', faction = 'UEF'},
+	{text = "[Technician]: We're already on it sir, we saw where the missile hit. We're assessing and repairing the damage but it'll take some time.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_09', faction = 'UEF'},
+	{text = "[Nichols]: Understood. Sir, you'll need to defend the ship until our engines are repaired. Take out the enemy missile launcher before it fires again.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_10', faction = 'UEF'},
+}
+
+M1_Tac_Dead = {
+	{text = "[Nichols]: The Missile Launcher is down, nice work sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_11', faction = 'UEF'},
+}
+
+UEFOutpost_Dead = {
+	{text = "[Parker]: That outpost meant nothing. This battle is far from over!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_03', faction = 'UEF'},
+	{text = "[Nichols]: Excellent work sir, the outpost is down.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_12', faction = 'UEF'},
+}
+
+M2_Update_Commander = {
+	{text = "[Nichols]: Our Technicians have located an enemy tech centre, and they say it's quite important. If you capture the tech centre, our techs can hook in and find out what's going on. Get an Engineer out to capture it sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_13', faction = 'UEF'},
+	{text = "[Nichols]: Sir, we've located an enemy factory that we can capture. It'll help you fight the enemy and use their own weapons against them. We've marked it on your radar. Nichols out.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_14', faction = 'UEF'},
+}
+
+M2TechCaptured = {
+	{text = "[Nichols]: Good job sir, our Technicians are hooking in to the database.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_15', faction = 'UEF'},
+}
+
+M3_Update_Commander = {
+	{text = "[Nichols]: It's time to fight back. Scans have revealed an enemy base to the Northwest. The base is shielded, and the Enemy Commander has a handful of patrols guarding his base.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_16', faction = 'UEF'},
+	{text = "[Nichols]: We've also discovered a small Power station to the North, we believe that's keeping the shields active in his base.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_17', faction = 'UEF'},
+	{text = "[Nichols]: Destorying the Power Station will make our assault easier.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_18', faction = 'UEF'},
+	{text = "[Nichols]: The Enemy Commander is increasing the rate of his attacks sir, stay alert. Nichols out.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_19', faction = 'UEF'},	
+}
+
+M3_ProtectFactory = {
+	{text = "[Nichols]: Sir, that captured Factory is vulnerable. We need it to keep our advantage. Defend it, Nichols out.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_20', faction = 'UEF'},	
+}
+
+M3FactoryDead = {
+	{text = "[Nichols]: We've lost the captured Factory sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_21', faction = 'UEF'},
+	{text = "[Parker]: There goes your only chance of stopping me! You're next, terrorist.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_04', faction = 'UEF'},
+}
+
+M3ShieldsDead = {
+	{text = "[Parker]: There goes your only chance of stopping me! You're next, terrorist.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_04', faction = 'UEF'},
+}
+
+EnemyDead = {
+	{text = "[Parker]: No! I will not be defea--", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_DEAD', faction = 'UEF'},
+	{text = "[Nichols]: Nice work sir, now we can get out of here.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_22', faction = 'UEF'},
+}
+
+Tech2AirFactoryBuilt = {
+	{text = "[Nichols]: The Tech 2 Air Factory has given you access to our Air Strike sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_23', faction = 'UEF'},
+}
+
+OrbStrikeReady = {
+	{text = "[Nichols]: Air strike is now available sir.", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_N_24', faction = 'UEF'},
+}
+
+UEFTauntFactory = {
+	{text = "[Parker]: You really think that'll help you? I'll destroy that Factory before you can use it!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'NMCA_P_05', faction = 'UEF'},
+}
+
+TAUNT1 = {
+	{text = "[Parker]: You will never escape!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT1', faction = 'UEF'},
+}
+
+TAUNT2 = {
+	{text = "[Parker]: I will make you suffer!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT2', faction = 'UEF'},
+}
+
+TAUNT3 = {
+	{text = "[Parker]: That ship will never take off!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT3', faction = 'UEF'},
+}
+
+TAUNT4 = {
+	{text = "[Parker]: Your resistance is useless. I will destroy you!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT4', faction = 'UEF'},
+}
+
+TAUNT5 = {
+	{text = "[Parker]: You have no chance against me!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT5', faction = 'UEF'},
+}
+
+TAUNT6 = {
+	{text = "[Parker]: Your days are numbered, I will rain supreme!", vid = 'C03_Berry_M03_0454.sfd', bank = 'NMCA_001', cue = 'UEFTAUNT6', faction = 'UEF'},
+}
