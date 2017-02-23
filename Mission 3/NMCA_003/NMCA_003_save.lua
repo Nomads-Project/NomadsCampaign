bd046781afe9975f96130f42420c00e30aec7032
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-23 23:22:17 +0100

    add mission 3

---
+++ Mission 3/NMCA_003/NMCA_003_save.lua
@@ -0,0 +1,55 @@
+--[[                                                                           ]]--
+--[[  Automatically generated code (do not edit)                               ]]--
+--[[                                                                           ]]--
+--[[                                                                           ]]--
+--[[  Scenario                                                                 ]]--
+--[[                                                                           ]]--
+Scenario = {
+    next_area_id = '4',
+    --[[                                                                           ]]--
+    --[[  Props                                                                    ]]--
+    --[[                                                                           ]]--
+    Props = {
+    },
+    --[[                                                                           ]]--
+    --[[  Areas                                                                    ]]--
+    --[[                                                                           ]]--
+    Areas = {
+        ['AREA_1'] = {
+            ['rectangle'] = RECTANGLE( 0, 0, 0, 0 ),
+        },
+    },
+    --[[                                                                           ]]--
+    --[[  Markers                                                                  ]]--
+    --[[                                                                           ]]--
+    MasterChain = {
+        ['_MASTERCHAIN_'] = {
+            Markers = {
+            },
+        },
+    },
+    Chains = {
+    },
+    --[[                                                                           ]]--
+    --[[  Orders                                                                   ]]--
+    --[[                                                                           ]]--
+    next_queue_id = '1',
+    Orders = {
+    },
+    --[[                                                                           ]]--
+    --[[  Platoons                                                                 ]]--
+    --[[                                                                           ]]--
+    next_platoon_id = '1',
+    Platoons = 
+    {
+    },
+    --[[                                                                           ]]--
+    --[[  Armies                                                                   ]]--
+    --[[                                                                           ]]--
+    next_army_id = '5',
+    next_group_id = '1',
+    next_unit_id = '2',
+    Armies = 
+    {
+    },
+}
---
+++ Mission 3/NMCA_003/NMCA_003_scenario.lua
@@ -0,0 +1,21 @@
+version = 3
+ScenarioInfo = {
+    name = 'nomad_mission_3',
+    description = '',
+    type = 'skirmish',
+    starts = true,
+    preview = '',
+    size = {1024, 1024},
+    map = '/maps/NMCA_003/NMCA_003.scmap',
+    save = '/maps/NMCA_003/NMCA_003_save.lua',
+    script = '/maps/NMCA_003/NMCA_003_script.lua',
+    norushradius = 0.000000,
+    Configurations = {
+        ['standard'] = {
+            teams = {
+                { name = 'FFA', armies = {} },
+            },
+            customprops = {
+            },
+        },
+    }}
