a8cb6c65c1fbc7e3bb7c410d6f8a9b9955b57804
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-02-23 23:12:24 +0100

    add mission 2 map files

---
+++ Mission 2/NMCA_002/NMCA_002_scenario.lua
@@ -0,0 +1,12 @@
+version = 3ScenarioInfo = {    name = 'nomad_mission_2',
+    description = "",
+    map_version = 1,
+    AdaptiveMap = false,
+    type = 'skirmish',    starts = true,    size = {1024, 1024},
+    map = '/maps/NMCA_002/NMCA_002.scmap',
+    save = '/maps/NMCA_002/NMCA_002_save.lua',
+    script = '/maps/NMCA_002/NMCA_002_script.lua',
+    preview = '',
+    norushradius = 0.000000,
+    Configurations = {        ['standard'] = {            teams = {                { 					name = 'FFA', 					armies = {}
+				},            },            customprops = {            },        },    }}
