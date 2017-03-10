a67f375b8a69e97ea8d2b3411ad969175bef5371
CookieNoob <sir-tobi.tolksdorf@gmx.net>
2017-03-10 23:35:52 +0100

    add mission 4
    adds objectives, missionplan and concept map

---
+++ Mission 4/NMCA_004/NMCA_004_scenario.lua
@@ -0,0 +1,16 @@
+version = 3ScenarioInfo = {    name = 'Nomad mission 4',
+    description = "",
+    map_version = 1,
+    AdaptiveMap = false,
+    type = 'skirmish',    starts = true,    size = {1024, 1024},
+    map = '/maps/NMCA_004/NMCA_004.scmap',
+    save = '/maps/NMCA_004/NMCA_004_save.lua',
+    script = '/maps/NMCA_004/NMCA_004_script.lua',
+    preview = '',
+    norushradius = 0.000000,
+    norushoffsetX_ARMY_1 = 0.000000,
+    norushoffsetY_ARMY_1 = 0.000000,
+    norushoffsetX_ARMY_2 = 0.000000,
+    norushoffsetY_ARMY_2 = 0.000000,
+    Configurations = {        ['standard'] = {            teams = {                { 					name = 'FFA', 					armies = {}
+				},            },            customprops = {            },        },    }}
